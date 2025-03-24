374981979a222065d1e0b604e0512cd9ccbc4c3d
MRKWEBANE <162419701+MRKWEBANE@users.noreply.github.com>
2025-03-24 12:55:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use("/api/auth", authRoutes);
+
 const PORT = 5000;
-
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
